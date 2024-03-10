35fe2f6135e7ecb13b0a28dd78f72a7631b798b4
Chrysille <125237029+Chrysille@users.noreply.github.com>
2024-03-10 19:55:50 +0800

    Error Validation
    Negative inputs and String inputs

--- sadnum.asm
+++ sadnum.asm
@@ -1,28 +1,30 @@
 %include "io64.inc"
 section .data
-    inputNum dq 0
+    
     currDigit dq 0
     remNum dq 0
     isSadNumber db 0
     continuePrompt db "Do you want to continue (Y/N)? ", 0
+    strVar dq 0
+    hasChar db 0
+    isNeg db 0
+    decVar dq 0
 
 section .text
     global main
     main:
     ; loop for continuing or exiting
     CONTINUE_LOOP:
-        ; accept integer input
-        PRINT_STRING "Input Number: "
-        GET_DEC 8, [inputNum]
-
+        ; accept string input
+        call ACCEPT_INPUT
         ; initialize values
-        MOV rax, [inputNum]      ; container for remaining numbers
+        MOV rax, [decVar]      ; container for remaining numbers
         MOV r8, 10          ; divisor
         MOV rdx, 0          ; remainder, current num to square
         MOV r10, 0          ; counter for loop iterations
         MOV byte [isSadNumber], 1 ; Assume it's a sad number until proven otherwise
         PRINT_STRING "Iterations: "   ; print sum in r9
-        PRINT_DEC 8, [inputNum]
+        PRINT_DEC 8, [decVar]
         ; loop
     L1:
         PRINT_STRING ","
@@ -94,7 +96,7 @@ section .text
         NEWLINE
         xor rax, rax
         ret
-
+;function list:
     GET_SQUARE:
         ; rdx -> remainder/current digit; rax-> digits left
         DIV r8  
@@ -114,3 +116,80 @@ section .text
         JNZ GET_SQUARE          ; If not zero, continue processing digits
 
         ret
+
+    ACCEPT_INPUT:
+        GET_STRING [strVar], 10 ;get string
+        MOV r11b, [strVar]
+        CMP r11b, 45    ;negative
+        JE INVALID
+        
+        ;check if within 48 - 57 range
+        MOV rsi, 0
+        
+        PARSE_STR:
+            MOV r11b, [strVar + rsi]
+            CMP r11b, 0
+            ;if zero terminate loop and convert to decimal value
+            JE CONVERT
+            
+            call CHECK_CHAR
+            CMP byte [hasChar], 1 ;check if may character na
+            JE INVALID
+            
+            ;at this point it means ok pa to continue
+            INC rsi
+            JMP PARSE_STR
+            
+        ;convert string to decimal equivalent
+        CONVERT:
+            call CONVERT_DEC
+            NEWLINE
+            ;PRINT_STRING "FINAL vALUE:"
+            PRINT_DEC 8, [decVar]
+            JMP END_INPUT_LOOP
+        
+        INVALID:
+        PRINT_STRING "Invalid Input!"
+        END_INPUT_LOOP:
+        ret
+    ;checks if character is a number from 0-9 or not
+    CHECK_CHAR:
+        CMP r11b, 48
+        JL character
+        CMP r11b, 57
+        JG character
+        ;at this point, you are sure that its a number
+        JMP end
+        character:
+            MOV byte [hasChar], 1
+        end:
+            ret
+            
+    ; converts string to decimal   
+    CONVERT_DEC:  
+        ;convert to decimal, store in r12
+        MOV rsi, 0
+        MOV r8, 10  
+        MOV r11, 0 
+        MOV r11b, [strVar]  ;move first digit to 8 bit register 
+        SUB r11, 48
+        MOV qword [decVar], r11
+        MOV rax, 0
+        loopConvert:
+            ;*10 + new digit
+            MOV al, r11b                ; move digit to al  
+            INC rsi                     ; increment loop counter
+            MOV r11,0
+            MOV r11b, [strVar + rsi]    ; move next digit to 8 bit reg
+            CMP r11b, 0
+            JE end_loopConvert          ; check if reach the end
+            
+            SUB r11b, 48
+            MOV rax, qword [decVar]
+            MUL r8                      ; multiply rax by 10
+            ADD rax, r11                ; if not end, add digit to rax
+            MOV qword [decVar], rax
+            JMP loopConvert
+            
+            end_loopConvert:
+            ret
--- test1.asm
+++ test1.asm
@@ -5,41 +5,31 @@ section .text
 global main
 main:
     ;write your code here
-    GET_STRING [var1], 10
-    CMP qword [var1], 65
+    GET_STRING [var1], 62
+    CMP qword [var1], 45
     ;A -> 65 Z-> 90 a->97 z-> 122
+    
     MOV RSI, 0
     ;check if 
     JE equal
-    PRINT_STRING "ITS NOT EQUALL"
+    PRINT_STRING "ITS NOT EQUAL"
     equal:
-        PRINT_STRING "ITS EQUALL"
+        PRINT_STRING "ITS EQUAL"
+    NEWLINE
+    MOV al, [var1+1]
+    CMP al, 0
+    JE yes
+        PRINT_STRING "not 0 terminated"
+        JMP end
+    yes:
+        PRINT_STRING "0 terminated"
         
-    PRINT_DEC 8, [var1]
-    DIVIDE:
-        MOV r10b, [var1 + rsi]
-        PRINT_DEC 1, r10b
-        INC 
-        
-    CONVERT_DEC:
-        ; rdx -> remainder/current digit; rax-> digits left
-        DIV r8  
-        ; update current digit and digits left
-        MOV qword [currDigit], rdx
-        MOV qword [remNum], rax
-
-        ; multiplication: MUL src; RAX*src->RDX:RAX
-        ; mov currDigit to rax
-        MOV rax, [currDigit]    ; copy current digit to rax
-        MUL rax                 ; square rax
-        ADD r9, rax             ; accumulate sum in r9
-
-        ; check if there are more digits to process
-        MOV rax, [remNum]      ; Move remaining digits to rax
-        CMP rax, 0             ; Check if rax is zero
-        JNZ GET_SQUARE          ; If not zero, continue processing digits
-
-        ret
+    end:
+    PRINT_CHAR [var1]
+    
+    PRINT_DEC 8, [var1+0]
+    
+    
     xor rax, rax
     ret
     
